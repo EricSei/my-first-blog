7b32aab6da3b1b9141c5ea17769d3360710f18e2
ericsei <nawnaw88@gmail.com>
2019-02-24 09:09:37 -0500

    Blog Form Updated

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,7 @@ from .forms import 	PostForm
 
 
 # Create your views here.
+# requst is passed by URL.py
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html',{'posts':posts})
