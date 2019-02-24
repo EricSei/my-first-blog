2f49477f85143568697a48d440d54e61ad0bd7f3
ericsei <nawnaw88@gmail.com>
2019-02-24 16:41:25 -0500

    Added Authentication

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
 	path('post/<int:pk>/', views.post_detail, name='post_detail'),#pk is primary key
 	path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 	path('post/new', views.post_new, name='post_new'),
-
-
 ]
